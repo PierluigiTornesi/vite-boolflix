3d390232c4f2207fb28fa7e02e1944de23f9ae2a
PierluigiTornesi <pierluigi.tornesi02@gmail.com>
2023-11-30 16:40:34 +0100

    add: visualizzazione immagini trovate e non

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -9,23 +9,41 @@
             }
         },
         methods:{
-            getImagePath(language){
+            getFlagPath(language){
                 return new URL(`../assets/images/${language}.png`, import.meta.url).href;
             },
             hasFlag(){
                 return this.flags.includes(this.movieObj.original_language)
+            },
+            getImagePath(path){
+                 return `https://image.tmdb.org/t/p/w342${path}`;
+            },
+            hasImagePath(path){
+                return (path !== null ? true : false)
+            },
+            imageNot(){
+                return "https://image.tmdb.org/t/p/"
             }
         }
     }
 </script>
 
 <template>
-  <section class="text-white text-left bg-dark border p-2">
+  <section  class="text-white text-left bg-dark border p-2 d-flex flex-column align-items-center">
+    <div>
+        <img v-if="hasImagePath(movieObj.poster_path)" :src="(getImagePath(movieObj.poster_path) )" alt="">
+        <img v-else src="../assets/images/noImage.jpg" alt="">
+    </div>
     <div class="dim">
         <div>Titolo: {{ movieObj.title ? movieObj.title : movieObj.name }}</div>
         <div>Titolo originale: {{ movieObj.original_title ? movieObj.original_title : movieObj.original_name }}</div>
         <div>
-            <img v-if="hasFlag()" :src="getImagePath(movieObj.original_language)" alt="">
+            <p v-if="hasFlag()">Lingua
+                <span>
+                    <img :src="getFlagPath(movieObj.original_language)" alt="">
+                </span>
+            </p>
+            <p v-else>Lingua: {{ movieObj.original_language }}</p>
         </div>
         <div>Voto: {{ Math.round(movieObj.vote_average /2) }}</div>
         <div>Overview: {{ movieObj.overview ? movieObj.overview : 'nessuna' }}</div>
@@ -37,10 +55,9 @@
 section{
     height: 100%;
 }
-
 .dim{
-    min-height: 300px;
-    width: 300px;
-    margin: 0 auto;
+        min-height: 300px;
+        width: 300px;
+        margin: 0 auto;   
 }
 </style>
